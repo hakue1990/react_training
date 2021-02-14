70ecf44b7b9f6ac6690703f8644b8e161d08c5af
Adam Hałdaś <adam.haldas@gmail.com>
2021-02-14 20:39:17 +0100

    test commit

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React from "react";
 import "./Form.css";
 
-
 const Form = (props) => {
-    return (
-        <>
-            <form autoComplete="off" onSubmit={props.submitFn} className='Form__wrapper'>
-                <h2>add new twitter account</h2>
-                <input required placeholder='name' name='name' />
-                <input required placeholder='link' link='link' />
-                <input placeholder='image' image='image' />
-                <textarea required placeholder='description' description='description' />
-                <button type='submit'>add item</button>
-            </form>
-        </>
-    );
-}
+  return (
+    <>
+      <form
+        autoComplete="off"
+        onSubmit={props.submitFn}
+        className="Form__wrapper">
+        <h2>add new twitter account</h2>
+        <input required placeholder="name" name="name" />
+        <input required placeholder="link" link="link" />
+        <input placeholder="image" image="image" />
+        <textarea
+          required
+          placeholder="description"
+          description="description"
+        />
+        <button type="submit">add itaem</button>
+      </form>
+    </>
+  );
+};
 
 export default Form;
