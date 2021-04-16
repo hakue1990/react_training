392623387c89ac6fff77bff450a2b34aa8e8f688
Adam <adam.haldas@gmail.com>
2021-04-16 08:40:13 +0200

    add class Form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,22 +4,31 @@ import Input from "../Input/Input";
 import Button from "../Button/Button";
 import Title from "../Title/Title";
 
-const Form = ({ submitFn }) => (
-  <div className={styles.wrapper}>
-    <Title>add new twitter account</Title>
-    <form autoComplete="off" className={styles.form} onSubmit={submitFn}>
-      <Input name="Name" label="Name" maxLength={30} />
-      <Input name="twitterLink" label="Twitter Link" maxLength={30} />
-      <Input name="image" label="Image" maxLength={30} />
-      <Input
-        tag="textarea"
-        name="description"
-        label="Description"
-        maxLength={200}
-      />
-      <Button>add new item</Button>
-    </form>
-  </div>
-);
+class Form extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div className={styles.wrapper}>
+        <Title>add new twitter account</Title>
+        <form
+          autoComplete="off"
+          className={styles.form}
+          onSubmit={this.props.submitFn}
+        >
+          <Input name="Name" label="Name" maxLength={30} />
+          <Input name="twitterLink" label="Twitter Link" maxLength={30} />
+          <Input name="image" label="Image" maxLength={30} />
+          <Input
+            tag="textarea"
+            name="description"
+            label="Description"
+            maxLength={200}
+          />
+          <Button>add new item</Button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Form;
