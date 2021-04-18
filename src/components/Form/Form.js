e599c8736a24d181cc9cc1604d4b3ace70beb934
Adam <adam.haldas@gmail.com>
2021-04-18 12:49:57 +0200

    add radio inputs to a Form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -14,6 +14,13 @@ class Form extends React.Component {
   state = {
     activeOption: types.twitter,
   };
+
+  handleRadioInputChange = (type) => {
+    this.setState({
+      activeOption: type,
+    });
+  };
+
   render() {
     return (
       <div className={styles.wrapper}>
@@ -23,6 +30,27 @@ class Form extends React.Component {
           className={styles.form}
           onSubmit={this.props.submitFn}
         >
+          <input
+            id={types.twitter}
+            type="radio"
+            onChange={() => this.handleRadioInputChange(types.twitter)}
+            checked={this.state.activeOption === types.twitter}
+          />
+          <label htmlFor={types.twitter}>Twitter</label>
+          <input
+            id={types.articles}
+            type="radio"
+            onChange={() => this.handleRadioInputChange(types.articles)}
+            checked={this.state.activeOption === types.articles}
+          />
+          <label htmlFor={types.articles}>Articles</label>
+          <input
+            id={types.notes}
+            type="radio"
+            onChange={() => this.handleRadioInputChange(types.notes)}
+            checked={this.state.activeOption === types.notes}
+          />
+          <label htmlFor={types.notes}>Notes</label>
           <Input name="Name" label="Name" maxLength={30} />
           <Input name="twitterLink" label="Twitter Link" maxLength={30} />
           <Input name="image" label="Image" maxLength={30} />
