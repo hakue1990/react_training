f29e67c11a820642c1355fa16a2111ecab0cf3cb
Adam <adam.haldas@gmail.com>
2021-05-11 17:36:25 +0200

    add new design

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from './views/Root/Root';
-
-ReactDOM.render(<Root />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import Root from "./views/Root/Root";
+import "./index.css";
+ReactDOM.render(<Root />, document.getElementById("root"));
