e0e474c471f46a75801d4c2b02f75067a036cde3
Adam <adam.haldas@gmail.com>
2021-05-12 15:57:57 +0200

    add add item fn

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -2,27 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./ListItem.module.scss";
 
-const ListItem = ({ rezerwacje, zamowienia, zadania }) => {
+const ListItem = ({ title, zamowienia, zadania }) => {
   const ImageTag = rezerwacje ? "img" : "div";
 
   return (
     <li className={styles.wrapper}>
-      <ImageTag
-        src={image}
-        className={image ? styles.image : styles.imageNone}
-        alt={name}
-      />
       <div>
-        <h2 className={styles.name}>{name}</h2>
-        <p className={styles.description}>{zamowienia}</p>
-        <a
-          href={zadania}
-          target="_blank"
-          className={styles.button}
-          rel="noopener noreferrer"
-        >
-          visit twitter page
-        </a>
+        <h2>{title}</h2>
       </div>
     </li>
   );
