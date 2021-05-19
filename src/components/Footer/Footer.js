1316b456ded50bfa68bc90f47d7e12a75a544e9e
Adam <adam.haldas@gmail.com>
2021-05-19 20:46:49 +0200

    change Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,45 @@ import styles from "./Footer.module.scss";
 const Footer = () => {
   return (
     <div className={styles.wrapper}>
-      <h3>made by AH, MK, MK, MS - k32.2 for Laboratorium inzynierskie WSB</h3>
+      <div className={styles.container}>
+        <div className={styles.row}>
+          {/*column1*/}
+          <div className={styles.col}>
+            <h4>Table Book inc</h4>
+            <ul className={styles.list}>
+              <li>736 082 373</li>
+              <li>Poznań, Poland</li>
+            </ul>
+          </div>
+          {/*column2*/}
+          <div className={styles.col}>
+            <h4>Stuff</h4>
+            <ul className={styles.list}>
+              <li>Adam Hałdaś</li>
+              <li>Mateusz Kokot</li>
+              <li>Michał Kolanowski</li>
+              <li>Michał Sowiński</li>
+            </ul>
+          </div>
+          {/*column3*/}
+          <div className={styles.col}>
+            <h4>Well another column xD</h4>
+            <ul className={styles.list}>
+              <li>I don't know yet</li>
+              <li>What</li>
+              <li>to write</li>
+              <li>here</li>
+            </ul>
+          </div>
+        </div>
+        <hr />
+        <div className={styles.rowSecond}>
+          <p>
+            &copy;{new Date().getFullYear()} TABLEBOOK INC | All right reserved
+            | Terms of Service | Privacy Policy
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
